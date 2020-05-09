cbefd38a4dcfe4d4d7b4deab2bc35b3dff39d81b
saisriharshap <saisreeharshap@gmail.com>
2020-05-09 17:46:22 +0530

    Day08_ValidPerfectSquare

---
+++ LeetCode/May 2020 Challenge/Day 08 - Valid Perfect Square/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1)
+            return true;
+        long low = 1;
+        long high = num/2;
+        while(low<=high){
+            long mid = (low+(high-low)/2);
+            if((mid*mid)==num)
+                return true;
+            else if((mid*mid)>num)
+                high = mid-1;
+            else if((mid*mid)<num)
+                low = mid+1;
+        }
+        return false;
+    }
+}
